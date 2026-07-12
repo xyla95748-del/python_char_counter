92cccaa2d3d88f4cab08fd12f40afbf30383631b
xyla <xyla95748@gmail.com>
2026-07-12 23:01:18 +0300

    Add files via upload

---
+++ main(4).py
@@ -0,0 +1,14 @@
+print("___Hello World")
+years = int(input("Enter your years: \n"))
+month_1 = int(input("Enter your month:\n "))
+age = 2026 - years
+month =( 7 - month_1) + age * 12
+age = month //12
+month%=12
+print("your age is : "+str(age) +"and month is :"+str(month))
+
+
+         
+
+
+
